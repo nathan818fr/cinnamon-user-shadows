00c95add0661ac6ad555637c17188e71b160d083
Nathan Poirier <nathan@poirier.io>
2023-09-13 12:15:17 +0200

    Use prettier to format all files
    Update .po files to match
    Update push_to_spices.sh to only include explicitly listed files
    
    Update .po files

--- scripts/push_to_spices.sh
+++ scripts/push_to_spices.sh
@@ -6,35 +6,38 @@ EXT_UUID='user-shadows@nathan818fr'
 EXT_URL='https://github.com/nathan818fr/cinnamon-user-shadows'
 
 function main() {
-    local local_repo; local_repo="$(realpath -m -- "$(dirname "$(realpath -m -- "$0")")/..")"
-    local spices_repo; spices_repo="$(realpath -m -- "${SPICES_EXTENSIONS_DIR:-${local_repo}/../cinnamon-spices-extensions}")"
+  local local_repo
+  local_repo="$(realpath -m -- "$(dirname "$(realpath -m -- "$0")")/..")"
+  local spices_repo
+  spices_repo="$(realpath -m -- "${SPICES_EXTENSIONS_DIR:-${local_repo}/../cinnamon-spices-extensions}")"
 
-    if ! git -C "$spices_repo" rev-parse --is-inside-work-tree >/dev/null; then
-        echo "warn: '${spices_repo}' is not a git repository" >&2
-        exit 1
-        # eg:
-        # $ git clone git@github.com:nathan818fr/cinnamon-spices-extensions.git
-        # $ git remote add linuxmint https://github.com/linuxmint/cinnamon-spices-extensions
-    fi
+  if ! git -C "$spices_repo" rev-parse --is-inside-work-tree > /dev/null; then
+    echo "warn: '${spices_repo}' is not a git repository" >&2
+    exit 1
+    # eg:
+    # $ git clone git@github.com:nathan818fr/cinnamon-spices-extensions.git
+    # $ git remote add linuxmint https://github.com/linuxmint/cinnamon-spices-extensions
+  fi
 
-    pushd "$spices_repo" >/dev/null
-    
-    git checkout master
-    git pull linuxmint master
-    git branch -D "$EXT_UUID" 2>/dev/null || true
-    git checkout -b "$EXT_UUID"
+  pushd "$spices_repo" > /dev/null
 
-    rm -rf -- "$EXT_UUID"
-    rsync -a --inplace --exclude={'/.git/','/scripts/','/.editorconfig'} -- "${local_repo}/" "${EXT_UUID}/"
-    git add -- "$EXT_UUID"
-    git status
+  git checkout master
+  git pull linuxmint master
+  git branch -D "$EXT_UUID" 2> /dev/null || true
+  git checkout -b "$EXT_UUID"
 
+  rm -rf -- "$EXT_UUID"
+  rsync -a --inplace --include={'/LICENSE','/README.md','/screenshot.png','/info.json','/files/','/files/**'} --exclude='*' -- "${local_repo}/" "${EXT_UUID}/"
+  git add -- "$EXT_UUID"
+  git status
+  if [[ -n "$(git status --porcelain)" ]]; then
     git commit -m "Update ${EXT_UUID}
-    
-Changelog at: ${EXT_URL}/commits/master"
-    git push -f --set-upstream -- origin "$EXT_UUID"
 
-    popd >/dev/null
+Changelog at: ${EXT_URL}/commits/main"
+  fi
+  git push -f --set-upstream -- origin "$EXT_UUID"
+
+  popd > /dev/null
 }
 
 main "$@"
